75b4d22ed2bdd6c1ec19e469a60e95b27a0a4b38
Sarthak agarwal <agarwalsarthak121@gmail.com>
2016-07-06 13:53:11 +0530

    Update tpd.py
    Can download any tutorial from tutorialspoint.com. IndexError issue solved.

--- tpd.py
+++ tpd.py
@@ -8,88 +8,25 @@ url = input('Enter url of the tutorial to download: ')
 name = input('Create new folder: ')
 os.makedirs(name,exist_ok=True)
 res = requests.get(url)
-soup = BeautifulSoup(res.text)
+soup = BeautifulSoup(res.text,'html5lib')
 ul = soup.findAll('ul',{'class':'nav nav-list primary left-menu'})
+print (len(ul))
 
-if len(ul) == 2:
-	li = ul[0].find_all('li')
-	for i in range(1,len(li)):
-		href = li[i].find('a').get('href')
+for i in range(len(ul)):
+	li = ul[i].find_all('li')
+	for j in range(1,len(li)):
+		href = li[j].find('a').get('href')
 		new_url = 'http://www.tutorialspoint.com'+href
 		res = requests.get(new_url)
-		soup = BeautifulSoup(res.text,'lxml')
+		soup = BeautifulSoup(res.text,'html5lib')
 		pdf = soup.select('.pdf-btn a')
-		url_pdf = 'http://www.tutorialspoint.com'+pdf[0].get('href')
-		res = requests.get(url_pdf)
-		print ('Downloading page '+str(i))
-		pdffile = open(os.path.join(name,os.path.basename(url_pdf)),'wb')
-		pdffile.write(res.content)
-		pdffile.close()
-elif len(ul) == 3:
-	li = ul[0].find_all('li')
-	li1 = ul[1].find_all('li')
-	for i in range(1,len(li)):
-		href = li[i].find('a').get('href')
-		new_url = 'http://www.tutorialspoint.com'+href
-		res = requests.get(new_url)
-		soup = BeautifulSoup(res.text,'lxml')
-		pdf = soup.select('.pdf-btn a')
-		url_pdf = 'http://www.tutorialspoint.com'+pdf[0].get('href')
-		res = requests.get(url_pdf)
-		print ('Downloading page '+str(i))
-		pdffile = open(os.path.join(name,os.path.basename(url_pdf)),'wb')
-		pdffile.write(res.content)
-		pdffile.close()
-	for i in range(1,len(li1)):
-		href = li1[i].find('a').get('href')
-		new_url = 'http://www.tutorialspoint.com'+href
-		res = requests.get(new_url)
-		soup = BeautifulSoup(res.text,'lxml')
-		pdf = soup.select('.pdf-btn a')
-		url_pdf = 'http://www.tutorialspoint.com'+pdf[0].get('href')
-		res = requests.get(url_pdf)
-		print ('Downloading page '+str(i))
-		pdffile = open(os.path.join(name,os.path.basename(url_pdf)),'wb')
-		pdffile.write(res.content)
-		pdffile.close()
-
-elif len(ul) == 4:
-	li = ul[0].find_all('li')
-	li1 = ul[1].find_all('li')
-	li2 = ul[2].find_all('li')
-	for i in range(1,len(li)):
-		href = li[i].find('a').get('href')
-		new_url = 'http://www.tutorialspoint.com'+href
-		res = requests.get(new_url)
-		soup = BeautifulSoup(res.text,'lxml')
-		pdf = soup.select('.pdf-btn a')
-		url_pdf = 'http://www.tutorialspoint.com'+pdf[0].get('href')
-		res = requests.get(url_pdf)
-		print ('Downloading page '+str(i))
-		pdffile = open(os.path.join(name,os.path.basename(url_pdf)),'wb')
-		pdffile.write(res.content)
-		pdffile.close()
-	for i in range(1,len(li1)):
-		href = li1[i].find('a').get('href')
-		new_url = 'http://www.tutorialspoint.com'+href
-		res = requests.get(new_url)
-		soup = BeautifulSoup(res.text,'lxml')
-		pdf = soup.select('.pdf-btn a')
-		url_pdf = 'http://www.tutorialspoint.com'+pdf[0].get('href')
-		res = requests.get(url_pdf)
-		print ('Downloading page '+str(i))
-		pdffile = open(os.path.join(name,os.path.basename(url_pdf)),'wb')
-		pdffile.write(res.content)
-		pdffile.close()
-	for i in range(1,len(li2)):
-		href = li2[i].find('a').get('href')
-		new_url = 'http://www.tutorialspoint.com'+href
-		res = requests.get(new_url)
-		soup = BeautifulSoup(res.text,'lxml')
-		pdf = soup.select('.pdf-btn a')
-		url_pdf = 'http://www.tutorialspoint.com'+pdf[0].get('href')
-		res = requests.get(url_pdf)
-		print ('Downloading page '+str(i))
-		pdffile = open(os.path.join(name,os.path.basename(url_pdf)),'wb')
-		pdffile.write(res.content)
-		pdffile.close()
+		if len(pdf) != 0:
+			url_pdf = 'http://www.tutorialspoint.com'+pdf[0].get('href')
+			res = requests.get(url_pdf)
+			print ('Downloading page '+str(j))
+			pdffile = open(os.path.join(name,os.path.basename(url_pdf)),'wb')
+			pdffile.write(res.content)
+			pdffile.close()
+		else:
+			continue
+print ('Download Complete')
