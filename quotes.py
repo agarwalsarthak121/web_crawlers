6633cd2a378a50b5067e41e357788e2579de6411
Sarthak agarwal <agarwalsarthak121@gmail.com>
2018-06-14 15:15:40 +0530

    Add files via upload

--- quotes.py
+++ quotes.py
@@ -1,6 +1,6 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
-import pynotify
+from win10toast import ToastNotifier
 import random
 from time import sleep
 
@@ -9,19 +9,21 @@ vocab = fw.read()
 vocab = vocab.split('\n')
 
 def sendmessage(title, message):
-    pynotify.init("Test")
-    notice = pynotify.Notification(title, message)
-    notice.show()
+    #pynotify.init("Test")
+    #notice = pynotify.Notification(title, message)
+    #notice.show()
+    toaster = ToastNotifier()
+    toaster.show_toast(title,
+                   message,
+                   icon_path=None,
+                   duration=20)
     return
     
 while True:
     i = random.randint(0,2002)
-    if i % 2 == 0:
-        if len(vocab[i]) < 118:
-            sendmessage(vocab[i],vocab[i+1])
-            sleep(300)
-        else:
-            continue
+    if i % 2 == 0 and len(vocab[i]) < 170:
+        sendmessage(vocab[i+1],vocab[i])
+        sleep(300)
     else:
         continue
     
