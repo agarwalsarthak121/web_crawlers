2fbdf3ca989abfb70f1bfedf5ea21307e1b45429
Sarthak agarwal <agarwalsarthak121@gmail.com>
2019-07-26 18:52:31 +0530

    Update json2Schema.py

--- json2Schema.py
+++ json2Schema.py
@@ -25,16 +25,14 @@ def getJsonData(key,value):
         return tempDict
     if(isinstance(value,list)):
         tempDict = {}
-        data_dict = {"type":"array","items":[]}
+        data_dict = {"type":"array","items":{"oneOf":[]}}
         for val in value:
             data_dict2 = {"type":"object","properties":{}}
-            data_dict["items"].append(data_dict2)
+            data_dict["items"]["oneOf"].append(data_dict2)
             for key2,value2 in val.items():
                 data_dict2["properties"].update(getJsonData(key2,value2))         
         tempDict[key] = data_dict
         return tempDict
-    
-
 
 result = {"type":"object","properties":{}}
 for key,value in data.items():
@@ -44,5 +42,3 @@ with open(fileName.split('.')[0]+'_schema.json', 'w') as fp:
     json.dump(result, fp)
 
 print(fileName.split('.')[0]+'_schema.json'+' file generated successfully')
-
-
